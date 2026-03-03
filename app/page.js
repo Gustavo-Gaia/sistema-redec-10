7d24ef2b30319da76c84a9a7b46a520c75557a58
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-03 14:30:52 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "Arial"
+    }}>
+      <h1>Sistema REDEC 10 funcionando 🚀</h1>
+    </div>
+  );
+}
