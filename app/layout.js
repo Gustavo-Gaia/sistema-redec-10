74e0451e320178c96be7d0095a8a312d63bf4f44
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-03 16:07:14 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,22 @@
 import "./globals.css";
+import { useState } from "react";
 
 export const metadata = {
-  title: "Sistema REDEC 10",
+  title: "SISTEMA INTEGRADO REDEC 10 - Norte",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <div className="min-h-screen flex bg-slate-100">
+          <Sidebar />
+          <div className="flex-1 flex flex-col">
+            <Header />
+            <main className="p-6 md:p-10">{children}</main>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
