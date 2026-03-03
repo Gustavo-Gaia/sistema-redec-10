9730e382d4d7f954686241879bbc7b70e5800558
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-03 16:09:38 -0300

    Create Header.js

---
+++ app/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="bg-white shadow-md p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold text-slate-800">
+        SISTEMA INTEGRADO REDEC 10 - Norte
+      </h1>
+
+      <div className="text-sm text-slate-500">
+        Painel Administrativo
+      </div>
+    </header>
+  );
+}
