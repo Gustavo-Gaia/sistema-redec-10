3160cfc864cf37dc2bf269cbdd891cdf3d47093a
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-06 19:17:03 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,17 @@
-import "./globals.css"
-import Sidebar from "./Sidebar"
-import Header from "./Header"
-
-export const metadata = {
-  title: "Sistema Integrado REDEC 10 - Norte",
-  description: "Painel de Controle Defesa Civil",
-}
-
 export default function Layout({ children }) {
   return (
-    <html lang="pt-br">
+    <html>
       <body>
         <div className="layout">
           <Sidebar />
           <div className="content">
-            <Header />
+            <Header /> {/* O cabeçalho fica aqui, acima do conteúdo */}
             <main className="main">
               {children}
             </main>
-            <footer className="footer">
-              <p>REDEC 10 - Norte © 2026 | Sistema de Gestão de Defesa Civil</p>
-            </footer>
           </div>
         </div>
       </body>
     </html>
-  )
+  );
 }
