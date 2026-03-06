5494a48b7a45aa2dfd312d525a9797a3dc027ac7
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-06 19:16:34 -0300

    Update Header.js

--- app/Header.js
+++ app/Header.js
@@ -2,16 +2,15 @@ export default function Header({ isAuthenticated = true }) {
   return (
     <header className="header-integrado">
       <div className="brand-area">
-        <img src="/REDEC_10_NORTE_LOGO.png" alt="Logo" style={{ height: '40px' }} />
+        <img src="/REDEC_10_NORTE_LOGO.png" alt="Logo" className="header-logo" />
         <div className="brand-text">
           <h1>Sistema Integrado REDEC 10 - Norte</h1>
           <p>Defesa Civil - Governo do Estado</p>
         </div>
       </div>
-
       <button className="auth-btn">
         {isAuthenticated ? "Sair" : "Login"}
       </button>
     </header>
-  )
+  );
 }
