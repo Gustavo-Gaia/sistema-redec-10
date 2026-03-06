d0cc8c0b4803dc06f3a61dd7da962db567db38c1
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-06 18:36:53 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,61 +1,72 @@
-export default function Dashboard() {
+export default function Dashboard(){
 
-  return (
+return(
 
-    <div>
+<div>
 
-      <h2>Dashboard</h2>
+<h2>Painel Operacional</h2>
 
-      <div className="cards">
+<div className="cards">
 
-        <div className="card">
-          <div className="card-header green">
-            Monitoramento dos Rios
-          </div>
+<div className="card">
 
-          <div className="card-body">
-            <p>Nível Crítico: 3</p>
-            <p>Atualizado: 10:15</p>
-          </div>
-        </div>
+<div className="card-header green">
+🌊 Monitoramento de Rios
+</div>
 
-        <div className="card">
-          <div className="card-header blue">
-            Boletins e SEI
-          </div>
+<div className="card-body">
+<p>Nível crítico: 3</p>
+<p>Atualização: 10:15</p>
+</div>
 
-          <div className="card-body">
-            <p>Pendências: 5</p>
-            <p>Último boletim: 24/04/2024</p>
-          </div>
-        </div>
+</div>
 
-        <div className="card">
-          <div className="card-header orange">
-            Equipe REDEC 10
-          </div>
 
-          <div className="card-body">
-            <p>Membros: 12</p>
-            <p>2 em férias</p>
-          </div>
-        </div>
+<div className="card">
 
-        <div className="card">
-          <div className="card-header red">
-            Ocorrências
-          </div>
+<div className="card-header blue">
+📄 Boletins e SEI
+</div>
 
-          <div className="card-body">
-            <p>Municípios afetados: 5</p>
-            <p>Desalojados: 208</p>
-          </div>
-        </div>
+<div className="card-body">
+<p>Pendências: 5</p>
+<p>Último boletim: 24/04</p>
+</div>
 
-      </div>
+</div>
 
-    </div>
 
-  );
+<div className="card">
+
+<div className="card-header orange">
+👥 Equipe REDEC 10
+</div>
+
+<div className="card-body">
+<p>Servidores: 12</p>
+<p>Em campo: 4</p>
+</div>
+
+</div>
+
+
+<div className="card">
+
+<div className="card-header red">
+🚨 Ocorrências
+</div>
+
+<div className="card-body">
+<p>Municípios afetados: 5</p>
+<p>Desalojados: 208</p>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+)
 
 }
