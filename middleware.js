3dd4f7813c66e96c81fb2f3ad5a3008fbbe67e20
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-09 08:08:13 -0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
-export function middleware(request){
+export function middleware(request) {
 
   const url = request.nextUrl.clone()
 
@@ -8,9 +8,24 @@ export function middleware(request){
 
   const usuario = request.cookies.get("usuario")
 
-  if(!usuario && !isLogin){
-    url.pathname="/login"
+  if (!usuario && !isLogin) {
+    url.pathname = "/login"
     return NextResponse.redirect(url)
   }
 
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/monitoramento/:path*",
+    "/equipe/:path*",
+    "/boletins/:path*",
+    "/agenda/:path*",
+    "/viaturas/:path*",
+    "/municipios/:path*",
+    "/patrimonio/:path*",
+    "/configuracoes/:path*"
+  ]
 }
