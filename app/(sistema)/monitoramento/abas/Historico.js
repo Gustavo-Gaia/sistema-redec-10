64d3fb09131c15f861a81f601e8a3ec7062422cb
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-11 18:47:53 -0300

    Create Historico.js

---
+++ app/(sistema)/monitoramento/abas/Historico.js
@@ -0,0 +1,17 @@
+/* app/(sistema)/monitoramento/abas/Historico.js */
+
+"use client"
+
+export default function Historico() {
+
+  return (
+
+    <div className="text-slate-600">
+
+      Histórico de medições aparecerá aqui.
+
+    </div>
+
+  )
+
+}
