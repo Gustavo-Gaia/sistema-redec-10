c673ed0b73a3a2c9eb5f597625635fd0c190b377
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-11 18:44:49 -0300

    Create SituacaoAtual.js

---
+++ app/(sistema)/monitoramento/abas/SituacaoAtual.js
@@ -0,0 +1,17 @@
+/* app/(sistema)/monitoramento/abas/SituacaoAtual.js */
+
+"use client"
+
+export default function SituacaoAtual() {
+
+  return (
+
+    <div className="text-slate-600">
+
+      Situação atual das estações aparecerá aqui.
+
+    </div>
+
+  )
+
+}
