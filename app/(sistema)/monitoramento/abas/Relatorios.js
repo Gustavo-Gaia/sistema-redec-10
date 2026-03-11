cdf258615c5e5a4444bb3c186eb48186115617dc
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-11 18:50:39 -0300

    Create Relatorios.js

---
+++ app/(sistema)/monitoramento/abas/Relatorios.js
@@ -0,0 +1,17 @@
+/* app/(sistema)/monitoramento/abas/Relatorios.js */
+
+"use client"
+
+export default function Relatorios() {
+
+  return (
+
+    <div className="text-slate-600">
+
+      Relatórios aparecerão aqui.
+
+    </div>
+
+  )
+
+}
