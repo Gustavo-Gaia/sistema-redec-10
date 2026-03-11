c9015c87d137f93ba77b4f86dd79380b2758f3d4
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-11 18:55:44 -0300

    Create InserirMedicoes.js

---
+++ app/(sistema)/monitoramento/abas/InserirMedicoes.js
@@ -0,0 +1,17 @@
+/* app/(sistema)/monitoramento/abas/InserirMedicoes. */
+
+"use client"
+
+export default function InserirMedicoes() {
+
+  return (
+
+    <div className="text-slate-600">
+
+      Inserção manual de medições aparecerá aqui.
+
+    </div>
+
+  )
+
+}
