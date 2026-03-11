58246059094243c6fea5ee10a0493d2067dd6910
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-11 18:49:19 -0300

    Create MapaEstacoes.js

---
+++ app/(sistema)/monitoramento/abas/MapaEstacoes.js
@@ -0,0 +1,17 @@
+/* app/(sistema)/monitoramento/abas/MapaEstacoes.js */
+
+"use client"
+
+export default function MapaEstacoes() {
+
+  return (
+
+    <div className="text-slate-600">
+
+      Mapa das estações aparecerá aqui.
+
+    </div>
+
+  )
+
+}
