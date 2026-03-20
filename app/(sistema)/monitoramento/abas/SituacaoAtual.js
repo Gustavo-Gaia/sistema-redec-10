ed62b93fc7eb17e35bed5f74a2091f4cb73311f6
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-03-20 12:08:17 -0300

    Update SituacaoAtual.js

--- app/(sistema)/monitoramento/abas/SituacaoAtual.js
+++ app/(sistema)/monitoramento/abas/SituacaoAtual.js
@@ -4,9 +4,7 @@
 
 import dynamic from "next/dynamic"
 import { useMonitoramento } from "../MonitoramentoContext"
-
 import PainelEstacao from "../componentes/PainelEstacao"
-import LegendaStatus from "../componentes/LegendaStatus"
 
 // 🚨 mapa sem SSR
 const MapaMonitoramento = dynamic(
@@ -15,34 +13,22 @@ const MapaMonitoramento = dynamic(
 )
 
 export default function SituacaoAtual() {
-
   const { estacaoSelecionada } = useMonitoramento()
 
   return (
-
     <div className="relative w-full h-[calc(100vh-140px)]">
-
-      {/* MAPA */}
+      
+      {/* MAPA PRINCIPAL 
+          Agora ele já carrega internamente a LegendaStatus 
+          compacta que criamos.
+      */}
       <MapaMonitoramento />
 
-      {/* LEGENDA */}
-      <div className="absolute bottom-4 left-4 z-[500]">
-        <LegendaStatus />
-      </div>
-
-      {/* PAINEL */}
+      {/* PAINEL LATERAL 
+          Aparece quando uma estação é clicada.
+      */}
       <PainelEstacao />
 
-      {/* MENSAGEM INICIAL */}
-      {!estacaoSelecionada && (
-        <div className="absolute top-4 left-1/2 -translate-x-1/2 z-[500]">
-          <div className="bg-white/90 backdrop-blur px-4 py-2 rounded-xl shadow text-sm text-slate-700">
-            Clique em uma estação no mapa para visualizar os dados
-          </div>
-        </div>
-      )}
-
     </div>
-
   )
 }
