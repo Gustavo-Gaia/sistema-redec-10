13b544d4b3f765b8d764b2bd3621160850e68b88
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-04-06 12:52:42 -0300

    Update RelatorioAtual.js

--- app/(sistema)/monitoramento/abas/RelatorioAtual.js
+++ app/(sistema)/monitoramento/abas/RelatorioAtual.js
@@ -3,13 +3,8 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { createClient } from "@supabase/supabase-js"
+import { supabase } from "@/lib/supabase"
 import ModalRelatorioAtual from "../componentes/modais/ModalRelatorioAtual"
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
 
 export default function RelatorioAtual() {
 
