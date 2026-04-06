56ced1854bc1cbca61b0a30917dc6e02d576ae82
Gustavo-Gaia <gustavogaiacbmerj@gmail.com>
2026-04-06 12:11:26 -0300

    Update ModalRelatorio.js

--- app/(sistema)/monitoramento/componentes/modais/ModalRelatorio.js
+++ app/(sistema)/monitoramento/componentes/modais/ModalRelatorio.js
@@ -3,13 +3,8 @@
 "use client"
 
 import { useEffect, useState, useRef } from "react"
-import { createClient } from "@supabase/supabase-js"
+import { supabase } from "@/lib/supabase" // ✅ Uso do Cliente Único
 import { toPng } from "html-to-image"
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
 
 export default function ModalRelatorio({ dadosDaTela, estacoes, colunasVisiveis, onClose }) {
   const [historico, setHistorico] = useState({})
